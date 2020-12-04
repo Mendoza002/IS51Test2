d8bfb2a2ddb08c152cb3ee4c1145ae28a09c8de8
Bryan Mendoza <mendozatech@outlook.com>
2020-12-03 16:59:58 -0800

    finish structured english

---
+++ test_2.py
@@ -0,0 +1,10 @@
+"""
+The function of this program is to display the number of grades, the average grade, and the percentage of grades above average.
+This is done by using the text file "Final.txt" and using the data from within to get each answer
+First we would need the define the main of where we are extracing the data along with what we are focusing on, which is the scores.
+Then we extract the from the file and have it set to read,
+then we use equations to get the number of grades that are listed
+then we use the sum of all grades and divided it with the total numbers of grades to get the average grade,
+then finally we use the equations to figure out the percentage of grades that are above the average.
+and print out the results of each of the three functions.
+"""
